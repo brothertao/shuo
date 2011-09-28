015b3212eb1b300ab4ae9e12e3106e89cf5612bb
moven <brothertao@gmail.com>
2011-09-28 18:04:21 +0800

    add comment tree

--- js/script.js
+++ js/script.js
@@ -1,29 +1,91 @@
-$(document).ready(function() {
-  searchReddit();
-});
-if (window.top != window.self) return;
-var exclude = ['.xml', '.txt', '.jpg', '.png', '.avi', '.mp3', '.pdf', '.mpg'];
-function searchReddit() {
-  var curPath = window.location.href;
-  var end = curPath.substr( - 4);
-  for (x in exclude) {
-    if (end == x) {
-      return;
-    }
+chrome.extension.onRequest.addListener(function(action, sender, sendResponse) {
+  console.log(action);
+  if ('show' === action) {
+    //get_comments();
+    build_view();
   }
-  var queryURL = "http://reddit.local/search.json?sort=top&q=url:" + escape(curPath);
-  chrome.extension.sendRequest({
-    'action': 'get',
-    'url': queryURL
-  },
-  function(response) {
-    var data = JSON.parse(response);
-    var foundItem = data.data.children[0].data;
-    if (foundItem.url == curPath) {
-      createPanel(foundItem.title, 'http://reddit.local' + foundItem.permalink + '.compact?keep_extension=True');
-    }
   });
+function build_view() {
+  if ($(".RedditEmbed").length > 0) return;
+
+		var setting = {
+			async: {
+				enable: true,
+				url:"../asyncData/getNodes.php",
+				autoParam:["id", "name=n"],
+				otherParam:{"otherParam":"zTreeAsyncTest"},
+				dataFilter: filter
+			},
+			view: {expandSpeed:"",
+				addHoverDom: addHoverDom,
+				removeHoverDom: removeHoverDom,
+				selectedMulti: false
+			},
+			edit: {
+				enable: true
+			},
+			data: {
+				simpleData: {
+					enable: true
+				}
+			},
+			callback: {
+				beforeAsync: beforeAsync,
+				beforeRemove: beforeRemove,
+				beforeRename: beforeRename
+			}
+		};
+		function filter(treeId, parentNode, childNodes) {
+			if (!childNodes) return null;
+			for (var i=0, l=childNodes.length; i<l; i++) {
+				childNodes[i].name = childNodes[i].name.replace(/\.n/g, '.');
+			}
+			return childNodes;
+		}
+		function beforeAsync(treeId, treeNode) {
+			return treeNode ? treeNode.level < 5 : true;
+		}
+		function beforeRemove(treeId, treeNode) {
+			var zTree = $.fn.zTree.getZTreeObj("ctree");
+			zTree.selectNode(treeNode);
+			return confirm("确认删除 节点 -- " + treeNode.name + " 吗？");
+		}		
+		function beforeRename(treeId, treeNode, newName) {
+			if (newName.length == 0) {
+				alert("节点名称不能为空.");
+				return false;
+			}
+			return true;
+		}
+
+		var newCount = 1;
+		function addHoverDom(treeId, treeNode) {
+			var sObj = $("#" + treeNode.tId + "_span");
+			if ($("#addBtn_"+treeNode.id).length>0) return;
+			var addStr = "<button type='button' class='add2' id='addBtn_" + treeNode.id
+				+ "' title='add node' onfocus='this.blur();'></button>";
+			sObj.append(addStr);
+			var btn = $("#addBtn_"+treeNode.id);
+			if (btn) btn.bind("click", function(){
+				var zTree = $.fn.zTree.getZTreeObj("ctree");
+				zTree.addNodes(treeNode, {id:(100 + newCount), pId:treeNode.id, name:"new node" + (newCount++)});
+			});
+		};
+		function removeHoverDom(treeId, treeNode) {
+			$("#addBtn_"+treeNode.id).unbind().remove();
+		};
+  var RedditEmbed = $("<div />").attr({
+    'id': 'RedditEmbed'
+  });
+  var cTree = $("<ul>").attr({'id':'ctree', 'class': 'ztree'});
+  RedditEmbed.append(cTree);
+  $('body').append(RedditEmbed);
+  $.fn.zTree.init($("#ctree"), setting);
+  console.log('finish build view');
 }
+
+
+
 function toggleElement(elemName) {
   var element = $(elemName);
   if (element.style.display == 'none') {
@@ -34,10 +96,6 @@ function toggleElement(elemName) {
 }
 function createPanel(panelTitle, HNurl) {
     console.log(HNurl);
-  if ($(".RedditEmbed").length > 0) return;
-  var RedditEmbed = $("<div />").attr({
-    'id': 'RedditEmbed'
-  });
   var RedditSite = $("<iframe />").attr({
     'id': 'RedditSite',
     'src': HNurl
